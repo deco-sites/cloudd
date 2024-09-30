e7e0c9853e51fbc79993ccac60529a2fde89d739
Ved Nig <vednig12@gmail.com>
2024-09-30 10:06:31 +0000

    Publish 1 from staging into origin/main

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -9,11 +9,6 @@ interface Props {
   width?: number;
   height?: number;
   text?: string;
-  text2?: string;
-  href2?: string;
-  text3?: string;
-  href3?: string;
-
 
 }
 
@@ -22,8 +17,7 @@ function Footer({
     "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
   href = "https://deco.cx/",
   text = "Made with",
-  text2="",
-  text3="",
+
   alt = "Made with deco.cx",
   height = 20,
   width = 50,
@@ -45,25 +39,7 @@ function Footer({
           />
         )}
       </a>
-            <a
-        href={href2}
-        class="flex flex-row gap-1 items-center justify-center text-xs"
-        target="_blank"
-      >
-        {text2 && <p>{text2}</p>}
-        {image && (
-         
-        )}
-      </a>      <a
-        href={href3}
-        class="flex flex-row gap-1 items-center justify-center text-xs"
-        target="_blank"
-      >
-        {text3 && <p>{text3}</p>}
-        {image && (
-         
-        )}
-      </a>
+       
     </div>
   );
 }
