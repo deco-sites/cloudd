53b7189d97f44f17557edd24d5d9a8b1e3dd70c9
Ved Nig <vednig12@gmail.com>
2024-09-30 10:03:04 +0000

    Publish 4 from staging into origin/main

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -9,6 +9,12 @@ interface Props {
   width?: number;
   height?: number;
   text?: string;
+  text2?: string;
+  href2?: string;
+  text3?: string;
+  href3?: string;
+
+
 }
 
 function Footer({
@@ -16,15 +22,17 @@ function Footer({
     "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
   href = "https://deco.cx/",
   text = "Made with",
+  text2="",
+  text3="",
   alt = "Made with deco.cx",
   height = 20,
   width = 50,
 }: Props): JSX.Element | null {
   return (
-    <div class="py-8 lg:px-0 px-6">
+    <div class="py-8 lg:px-0 px-6 flex flex-row">
       <a
         href={href}
-        class="flex flex-row gap-1 items-center justify-center text-xs"
+        class=" gap-1 items-center justify-center text-xs"
         target="_blank"
       >
         {text && <p>{text}</p>}
@@ -35,6 +43,25 @@ function Footer({
             height={height || 20}
             width={width || 50}
           />
+        )}
+      </a>
+            <a
+        href={href2}
+        class="flex flex-row gap-1 items-center justify-center text-xs"
+        target="_blank"
+      >
+        {text2 && <p>{text2}</p>}
+        {image && (
+         
+        )}
+      </a>      <a
+        href={href3}
+        class="flex flex-row gap-1 items-center justify-center text-xs"
+        target="_blank"
+      >
+        {text3 && <p>{text3}</p>}
+        {image && (
+         
         )}
       </a>
     </div>
